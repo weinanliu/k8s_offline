9857d1bab0856ec1d76c22d9543116e0ec5ec832
刘炜楠 <liuweinan@liuweinandeMacBook-Pro.local>
2025-12-12 01:01:12 +0800

    docker install

--- 1.install_conatainerd.sh
+++ 1.install_conatainerd.sh
@@ -1,26 +1,7 @@
 #!/bin/bash -x
 #
 #
-# https://www.runoob.com/docker/ubuntu-docker-install.html
-#
-#
-sudo apt-get update
-
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
 
 
-curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu/ \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+#curl -fsSL https://get.docker.com -o install-docker.sh
+sudo sh install-docker.sh --mirror Aliyun
