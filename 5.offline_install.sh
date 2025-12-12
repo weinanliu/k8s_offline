57e8a78cd8caeff3c8d98187f7b81b2228b6a7c0
刘炜楠 <liuweinan@liuweinandeMacBook-Pro.local>
2025-12-12 13:07:05 +0800

    1

--- 5.offline_install.sh
+++ 5.offline_install.sh
@@ -7,17 +7,14 @@ sleep 10
 
 sudo swapoff -a
 
+sudo sed -i 's|sandbox_image = .*|sandbox_image = "registry.k8s.io/pause:3.10.1"|g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
 sudo systemctl enable --now kubelet
 
 for imgtar in *images.tar; do
   ctr -n k8s.io images import ${imgtar}
 done
-
-# kubeadm init的时候总有人想找k8s.io
-#ctr -n k8s.io images tag  registry.aliyuncs.com/google_containers/pause:3.10.1 registry.k8s.io/pause:3.10.1
-	#--control-plane-endpoint=10.211.55.4 \
-	#--apiserver-advertise-address=10.211.55.4 \
-
 
 kubeadm init  \
         --pod-network-cidr=192.168.0.0/16 \
